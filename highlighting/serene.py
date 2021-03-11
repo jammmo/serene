883c4c566d2d7f3e99b4545b8ce22ed39e5a7957
Jamie Moschella <jamie@uconn.edu>
2021-03-11 13:10:33 -0500

    Adds highlighing for 'bind' and archives old examples

--- highlighting/serene.py
+++ highlighting/serene.py
@@ -11,7 +11,7 @@ class SereneLexer(RegexLexer):
     tokens = {
         'root': [
             (r'\b(if|else|elseif|while|for|return|break|either|continue|match|is)\b', Keyword),
-            (r'\b(set|var|const|run|import|or|not|and|print|throw)\b', Keyword),
+            (r'\b(set|var|const|bind|run|import|or|not|and|print|throw)\b', Keyword),
             (r'\b(function|method|specifics|signatures|friend|constructor|interface|type|subscript|with|implements|struct|enum|tuple)\b', Keyword),
             (r'\b(mut|mutate|move|copy|maybe|defined|undefined|True|False)\b', Name.Constant),
             (r'\b(?<!\.)((?!True)(?!False)[A-Z]\w*)\b', Name.Class),
