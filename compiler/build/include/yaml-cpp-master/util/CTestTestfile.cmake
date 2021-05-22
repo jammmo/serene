d09c510c6a5fb465ca7501250effec1555347e8d
Jamie Moschella <jamie@uconn.edu>
2021-05-22 15:01:51 -0400

    Removes old C++ and reorganizes

--- compiler/build/include/yaml-cpp-master/util/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /cygdrive/c/Users/mosch/Desktop/Serene/compiler/include/yaml-cpp-master/util
-# Build directory: /cygdrive/c/Users/mosch/Desktop/Serene/compiler/build/include/yaml-cpp-master/util
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
