a6f626d77f2cffcf1225f961e7d029c1bd269a2a
Jamie Moschella <jamie@uconn.edu>
2021-07-11 18:51:31 -0400

    Initialize colorama

--- compiler/run_tests.py
+++ compiler/run_tests.py
@@ -1,10 +1,12 @@
 import subprocess
 from pathlib import Path
 from natsort import os_sorted
-from colorama import Fore, Style
+from colorama import Fore, Style, init as init_colorama
 
 # Directory of this file ( /serene/compiler/ )
 here = Path(__file__).parent.resolve()
+
+init_colorama()
 
 paths = os_sorted([Path(x) for x in here.glob("./tests/t*.sn")])
 for p in paths:
@@ -15,4 +17,4 @@ for p in paths:
         print(f"{Fore.GREEN}> Success!{Style.RESET_ALL}")
     else:
         print(f"{Fore.YELLOW}{serene_completed_process.stderr}{Style.RESET_ALL}")
-        print(f"{Fore.RED}{Style.BRIGHT}> Failed with error code {serene_completed_process.returncode}{Style.RESET_ALL}.")
+        print(f"{Fore.RED}{Style.BRIGHT}> Failed with error code {serene_completed_process.returncode}.{Style.RESET_ALL}")
